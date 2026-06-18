3415993e5616670be45f6ff803ab334b16d0bc16
Siri-cloud <kadali.Siri28@gmail.com>
2026-06-18 21:42:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,9 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+resource "aws_ecr_repository" "kronos_anomaly" {
+  name                 = "kronos-anomaly"
+  image_tag_mutability = "MUTABLE"
 
-  name               = var.cluster_name
-  kubernetes_version = var.cluster_version
-
-  vpc_cidr = "10.0.0.0/16"
-
-  azs = [
-    "ap-south-1a",
-    "ap-south-1b"
-  ]
-
-  private_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
-
-  public_subnets = [
-    "10.0.101.0/24",
-    "10.0.102.0/24"
-  ]
-
-  enable_irsa = true
-
-  eks_managed_node_groups = {
-    kronos_nodes = {
-      instance_types = [var.instance_type]
-
-      min_size     = var.min_size
-      max_size     = var.max_size
-      desired_size = var.desired_size
-    }
+  image_scanning_configuration {
+    scan_on_push = true
   }
 
   tags = {
